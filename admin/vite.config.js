58a7b4aa53354430d43ad589e6fa0af69b21d45e
Kriti <kritikumari36312@gmail.com>
2024-08-14 17:32:18 +0530

    feat: some changes

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // vite.config.js
+  server: {
+    port: 4000, // Specify your port here
+    host: '0.0.0.0', // Bind to 0.0.0.0 to allow external access
+  },
 })
+
